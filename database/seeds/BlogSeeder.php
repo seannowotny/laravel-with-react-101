2b45eeaa335a75d1dad197ce35e375cbe7ab1170
seannowotny <seannowotny@live.de>
2019-10-03 13:58:07 +0200

    Create Blog model

---
+++ database/seeds/BlogSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $blogCount = (int)$this->command->ask('How many blogs would you like?', 10);
+
+        factory(App\Blog::class, $blogCount)->create();
+    }
+}
